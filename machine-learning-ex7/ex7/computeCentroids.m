560554071f5e11ea2e88aeb2a2399e6d04d24b09
Lotfi Mira <lotfim@mirageoscience.com>
2017-11-24 20:46:11 -0500

    compute centroids

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,10 +26,17 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+for k = 1:K
+  
+bin_vector = (idx == k);
 
+total = sum(X .* bin_vector, 1);
 
+count = sum(bin_vector, 1);
 
+centroid(k,:) = total ./ count;
 
+end
 
 
 
